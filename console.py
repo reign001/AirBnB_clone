1b4bd9209ea38f2a7debc438cd9b9818ca59307a
reign001 <olochej543@gmail.com>
2024-01-10 14:46:17 -0800

    updated console

--- console.py
+++ console.py
@@ -1,1 +1,34 @@
 import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    this class defines the command line interpreter
+    """
+    prompt = ("hbnb")
+
+
+    def do_quit(self, arg):
+        """this quits or exits the programme"""
+        return True
+
+    do_EOF = do_quit
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+HBNBCommand().cmdloop()
