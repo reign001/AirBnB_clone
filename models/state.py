66ab298ddfb6bf4a8a4f6f10a61ae6a45fdd7120
reign001 <olochej543@gmail.com>
2024-01-13 17:30:35 -0800

    updated

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines the state class"""
+from models.base_model import BaseModel
+
+class state(BaseModel):
+    """Represents a state.
+        Attribute:
+        name(str): the name of a string"""
+
+        name = ""
